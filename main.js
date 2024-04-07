2b3135e9a84d2d368df2e49955fafd2271e93b84
Jimenez-Martinez-Victoria <edenvictoriajimenes@gmail.com>
2024-04-07 01:27:46 -0600

    feature(common):[lab2-files-evaluation]

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-import './style.css'
+const buyList = [
+  "books",
+  "bread",
+  "eraser",
+  "earphones",
+  "collection-cards",
+  "hdmi",
+  "vga",
+  "motherboard",
+  "university-books",
+];
+const rootDiv = document.getElementById('root');
+const buyListContainer = document.createElement('div');
+buyListContainer.id = 'buyListContainer';
+buyListContainer.innerHTML = `<ul>${buyList.map(item => `<li>${item}</li>`).join('')}</ul>`;
+rootDiv.insertAdjacentElement('afterend', buyListContainer);
+
+const link = document.getElementById('link');
+link.href = 'https://keybr.com';
 
 document.querySelector('#app').innerHTML = `
   <div style="display: flex; flex-direction: column;">
